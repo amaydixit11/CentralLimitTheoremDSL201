f9a9051d4b6b20297f74b4377c6085538a2c9516
amaydixit11 <amaydixit11@gmail.com>
2024-09-20 14:41:06 +0530

    added readme

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class CentralLimitTheorem:
         sample = random.choices(self.distribution, k=N)
         return np.mean(sample)
 
-    def run_sample(self, N, num_samples=1000):
+    def run_sample_demo(self, N, num_samples=1000):
         return [self._sample(N) for _ in range(num_samples)]
 
 def generate_distribution(distribution_type, size):
@@ -23,10 +23,10 @@ def generate_distribution(distribution_type, size):
         return list(np.random.normal(50, 15, size))
     elif distribution_type == 'exponential':
         return list(np.random.exponential(50, size))
-    
+
 
 def plot_distribution(distribution, title=None, bin_min=None, bin_max=None, num_bins=None, ax=None):
-    sns.histplot(distribution, bins=num_bins, kde=True, ax=ax)
+    sns.histplot(distribution, bins=num_bins, kde=True, color="skyblue", ax=ax)
     
     if title:
         ax.set_title(title)
@@ -43,10 +43,10 @@ def main():
 
     for i, dist_type in enumerate(distribution_types):
         sample_distribution = generate_distribution(dist_type, 10000)
-        clt = CentralLimitTheorem(sample_distribution)
+        clt_demo = CentralLimitTheorem(sample_distribution)
         
         for j, N in enumerate(n_vals):
-            means = clt.run_sample(N=N)
+            means = clt_demo.run_sample_demo(N=N)
             plot_distribution(means, f"{dist_type.capitalize()} - N = {N}", 
                               bin_min=min(sample_distribution), bin_max=max(sample_distribution), 
                               num_bins=40, ax=axes[i, j])
